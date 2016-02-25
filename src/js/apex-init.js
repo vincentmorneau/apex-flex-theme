57fc917ea0728b233b84d7793e87a739fec99613
Vincent Morneau <vincent.morneau@gmail.com>
2016-02-25 00:13:46 -0500

    Added Carousel

--- src/js/apex-init.js
+++ src/js/apex-init.js
@@ -2,5 +2,12 @@
 APEX Initialization
 ************/
 $(function() {
+    $(".carousel").each(function() {
+        var carousel = $(this);
 
+        carousel.find(".carousel-item").each(function(index) {
+            var attributes = (index === 0) ? "class=\"active\"" : "";
+            carousel.find(".carousel-indicators").append("<li " + attributes + " data-target=\"#" + $(carousel).attr("id") + "\" data-slide-to=\"" + index + "\"></li>");
+        });
+    });
 });
