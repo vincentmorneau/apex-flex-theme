926de29e9ad01f2a6a0b97803b28249dd7f2b321
Vincent Morneau <vincent.morneau@gmail.com>
2016-02-25 23:45:29 -0500

    Added collapsibles

--- src/js/apex-init.js
+++ src/js/apex-init.js
@@ -2,6 +2,7 @@
 APEX Initialization
 ************/
 $(function() {
+    // Carousel
     $(".carousel").each(function() {
         var carousel = $(this);
 
@@ -9,5 +10,22 @@ $(function() {
             var attributes = (index === 0) ? "class=\"active\"" : "";
             carousel.find(".carousel-indicators").append("<li " + attributes + " data-target=\"#" + $(carousel).attr("id") + "\" data-slide-to=\"" + index + "\"></li>");
         });
+    });
+
+    // Collapsible
+    $(".ab-region.collapse").attr({
+        "role":"tablist",
+        "aria-multiselectable":"true"
+    });
+
+    $("button[target]").each(function() {
+        if ($("#" + $(this).attr("target")).length > 0) {
+            $(this).attr({
+                "data-toggle": "collapse",
+                "data-target": "#" + $(this).attr("target"),
+                "aria-expanded": "false",
+                "aria-controls": $(this).attr("target")
+            });
+        }
     });
 });
