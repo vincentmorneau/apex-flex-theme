e8d4b0dc45aaebb0e73ea5734d09bef16319f1db
Vincent Morneau <vincent.morneau@gmail.com>
2016-03-06 13:13:02 -0500

    Added Scrollspy, sticky plugin and enhancements to navs

--- src/js/apex-init.js
+++ src/js/apex-init.js
@@ -1,7 +1,81 @@
+// Get unique id
+var guid = function() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return function() {
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
+    };
+};
+
+// Pushpin plugin
+(function($) {
+    $(document).ready(function() {
+        $.fn.pushpin = function(options) {
+            var defaults = {
+                top: 0,
+                bottom: Infinity,
+                offset: 0
+            };
+            options = $.extend(defaults, options);
+
+            $index = 0;
+            return this.each(function() {
+                var $uniqueId = guid(),
+                    $this = $(this),
+                    $original_offset = $(this).offset().top;
+
+                function removePinClasses(object) {
+                    object.removeClass('pin-top');
+                    object.removeClass('pinned');
+                    object.removeClass('pin-bottom');
+                }
+
+                function updateElements(objects, scrolled) {
+                    objects.each(function() {
+                        // Add position fixed (because its between top and bottom)
+                        if (options.top <= scrolled && options.bottom >= scrolled && !$(this).hasClass('pinned')) {
+                            removePinClasses($(this));
+                            $(this).css('top', options.offset);
+                            $(this).addClass('pinned');
+                        }
+
+                        // Add pin-top (when scrolled position is above top)
+                        if (scrolled < options.top && !$(this).hasClass('pin-top')) {
+                            removePinClasses($(this));
+                            $(this).css('top', 0);
+                            $(this).addClass('pin-top');
+                        }
+
+                        // Add pin-bottom (when scrolled position is below bottom)
+                        if (scrolled > options.bottom && !$(this).hasClass('pin-bottom')) {
+                            removePinClasses($(this));
+                            $(this).addClass('pin-bottom');
+                            $(this).css('top', options.bottom - $original_offset);
+                        }
+                    });
+                }
+
+                updateElements($this, $(window).scrollTop());
+                $(window).on('scroll.' + $uniqueId, function() {
+                    var $scrolled = $(window).scrollTop() + options.offset;
+                    updateElements($this, $scrolled);
+                });
+            });
+        };
+    });
+}(jQuery));
+
 /************
 APEX Initialization
 ************/
 $(function() {
+    // replace navbar brand
+    $(".ab-header nav.navbar").prepend($(".ab-header a.navbar-brand").removeClass("hidden-xs-up"));
+
     // Carousel
     $(".carousel").each(function() {
         var carousel = $(this);
@@ -14,8 +88,8 @@ $(function() {
 
     // Collapsible
     $(".ab-region.collapse").attr({
-        "role":"tablist",
-        "aria-multiselectable":"true"
+        "role": "tablist",
+        "aria-multiselectable": "true"
     });
 
     $("button[target]").each(function() {
@@ -50,6 +124,14 @@ $(function() {
         $(this).attr({
             "data-toggle": "popover",
             "data-content": $(this).data("help")
+        });
+    });
+
+    // Pushpin
+    $('.ab-sticky .navbar').each(function() {
+        $(this).pushpin({
+            top: $(this).offset().top,
+            offset: $(".ab-navbar-fixed-top .ab-header .navbar").outerHeight()
         });
     });
 });
--- src/js/bootstrap-init.js
+++ src/js/bootstrap-init.js
@@ -6,4 +6,8 @@ $(function() {
     $('[data-toggle="tooltip"]').tooltip(appSettings.tooltip);
     // Popovers
     $('[data-toggle="popover"]').popover(appSettings.popover);
+    // Scrollspy
+    $(".ab-list-scrollspy").each(function(){
+        $('body').scrollspy({ target: '#' + $(this).attr("id") })
+    });
 });
