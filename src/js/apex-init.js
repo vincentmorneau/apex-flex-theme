00f95ec8b0cea35ca3168907ef1baa00db3aa4f4
Vincent Morneau <vincent.morneau@gmail.com>
2018-02-12 00:36:11 -0500

    another huge chunk of work for bootstrap 4, mostly around navigation

--- src/js/apex-init.js
+++ src/js/apex-init.js
@@ -1,214 +1,196 @@
-// Get unique id
-var guid = function() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-    return function() {
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-            s4() + '-' + s4() + s4() + s4();
-    };
+/**
+ * Removes empty objects from the DOM based on the given selector
+ * @returns {object}
+ */
+$.fn.removeEmpty = function() {
+	return this.filter(function() {
+		return $.trim($(this).text()) === "" && $(this).children().length === 0;
+	}).remove();
 };
 
-// Pushpin plugin
-(function($) {
-    $(document).ready(function() {
-        $.fn.pushpin = function(options) {
-            var defaults = {
-                top: 0,
-                bottom: Infinity,
-                offset: 0
-            };
-            options = $.extend(defaults, options);
+/**
+ * Removes empty objects from the DOM based on the given selector
+ * @returns {object}
+ */
+$.fn.removeEmptySpaces = function() {
+	return this.filter(function() {
+		return $.trim($(this).text()) === "";
+	}).remove();
+};
 
-            $index = 0;
-            return this.each(function() {
-                var $uniqueId = guid(),
-                    $this = $(this),
-                    $original_offset = $(this).offset().top;
+/**
+ * @namespace apexBootstrap
+ **/
+var apexBootstrap = apexBootstrap || {};
 
-                function removePinClasses(object) {
-                    object.removeClass('pin-top');
-                    object.removeClass('pinned');
-                    object.removeClass('pin-bottom');
-                }
+/**
+ * @module debug
+ **/
+apexBootstrap.debug = {
+	time: function(name) {
+		if (apex.debug.getLevel() > apex.debug.LOG_LEVEL.OFF) {
+			console.time(name);
+		}
+	},
 
-                function updateElements(objects, scrolled) {
-                    objects.each(function() {
-                        // Add position fixed (because its between top and bottom)
-                        if (options.top <= scrolled && options.bottom >= scrolled && !$(this).hasClass('pinned')) {
-                            removePinClasses($(this));
-                            $(this).css('top', options.offset);
-                            $(this).addClass('pinned');
-                        }
+	timeEnd: function(name) {
+		if (apex.debug.getLevel() > apex.debug.LOG_LEVEL.OFF) {
+			console.timeEnd(name);
+		}
+	}
+};
 
-                        // Add pin-top (when scrolled position is above top)
-                        if (scrolled < options.top && !$(this).hasClass('pin-top')) {
-                            removePinClasses($(this));
-                            $(this).css('top', 0);
-                            $(this).addClass('pin-top');
-                        }
+/**
+ * @module theme
+ **/
+apexBootstrap.theme = {
+	bindings: function() {
+		var ViewModel = function() {
+			// This substitution string is driven from page template
+			// and then used in list templates
+			this.homeLink = ko.observable();
+			this.appName = ko.observable();
+		};
 
-                        // Add pin-bottom (when scrolled position is below bottom)
-                        if (scrolled > options.bottom && !$(this).hasClass('pin-bottom')) {
-                            removePinClasses($(this));
-                            $(this).addClass('pin-bottom');
-                            $(this).css('top', options.bottom - $original_offset);
-                        }
-                    });
-                }
+		var viewModel = new ViewModel();
+		ko.applyBindings(viewModel);
+	},
 
-                updateElements($this, $(window).scrollTop());
-                $(window).on('scroll.' + $uniqueId, function() {
-                    var $scrolled = $(window).scrollTop() + options.offset;
-                    updateElements($this, $scrolled);
-                });
-            });
-        };
-    });
-}(jQuery));
+	init: function() {
+		// Deletes empty html tags
+		$('.nav-item-text, i:not([class]), i[class=""]').removeEmpty();
 
-/************
-APEX Initialization
-************/
+		// Carousel
+		// $(".carousel").each(function() {
+		// 	var carousel = $(this);
+		//
+		// 	carousel.find(".carousel-item").each(function(index) {
+		// 		var attributes = (index === 0) ? "class=\"active\"" : "";
+		// 		carousel.find(".carousel-indicators").append("<li " + attributes + " data-target=\"#" + $(carousel).attr("id") + "\" data-slide-to=\"" + index + "\"></li>");
+		// 	});
+		// });
+
+		// Collapsible
+		// $(".ab-region.collapse").attr({
+		// 	"role": "tablist",
+		// 	"aria-multiselectable": "true"
+		// });
+
+		// $("button[target]").each(function() {
+		// 	if ($("#" + $(this).attr("target")).length > 0) {
+		// 		$(this).attr({
+		// 			"data-toggle": "collapse",
+		// 			"data-target": "#" + $(this).attr("target"),
+		// 			"aria-expanded": "false",
+		// 			"aria-controls": $(this).attr("target")
+		// 		});
+		// 	}
+		// });
+
+		// Tooltips
+		// $(".ab-field-tooltip [data-help][data-help!=''], .ab-button-tooltip[data-help][data-help!='']").each(function() {
+		// 	$(this).attr({
+		// 		"data-toggle": "tooltip",
+		// 		"title": $(this).data("help")
+		// 	});
+		// });
+
+		// Popovers
+		// $(".ab-field-popover [data-help][data-help!='']").each(function() {
+		// 	$(this).attr({
+		// 		"data-toggle": "popover",
+		// 		"data-content": $(this).data("help"),
+		// 		"title": $(this).text()
+		// 	});
+		// });
+
+		// $(".ab-button-popover[data-help][data-help!='']").each(function() {
+		// 	$(this).attr({
+		// 		"data-toggle": "popover",
+		// 		"data-content": $(this).data("help")
+		// 	});
+		// });
+
+		// Single Button Dropdowns
+		// $(".ab-single-button-dropdown").each(function() {
+		// 	$(this)
+		// 		.removeAttr("onclick")
+		// 		.addClass("dropdown-toggle")
+		// 		.attr({
+		// 			"data-toggle": "dropdown",
+		// 			"aria-haspopup": "true",
+		// 			"aria-expanded": "false",
+		// 		})
+		// 		.prependTo($(this).closest(".ab-region").find(".ab-btn-dropdown"));
+		// });
+
+		// Split Button Dropdowns
+		// $(".ab-split-button-dropdown").each(function() {
+		// 	var classes = ["btn-primary", "btn-secondary", "btn-success", "btn-info", "btn-warning", "btn-danger", "btn-primary-outline", "btn-secondary-outline", "btn-success-outline", "btn-info-outline", "btn-warning-outline", "btn-danger-outline"];
+		//
+		// 	var elementClasses = $(this).attr("class").split(" ");
+		//
+		// 	for (var i = 0; i < elementClasses.length; i++) {
+		// 		if ($.inArray(elementClasses[i], classes) >= 0) {
+		// 			$thisClass = elementClasses[i];
+		// 			break;
+		// 		}
+		// 	}
+		//
+		// 	var toggle = '<button type="button" class="btn ' + $thisClass + ' dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="sr-only">Toggle Dropdown</span></button>';
+		//
+		// 	$(this).prependTo($(this).closest(".ab-region").find(".ab-btn-dropdown").prepend(toggle));
+		// });
+
+		// Label
+		// $(".input-field label").each(function() {
+		// 	$(this).appendTo($(this).parent());
+		// });
+
+		// Input Groups
+		// $(".form-group").not(":only-child").each(function() {
+		// 	$(this)
+		// 		.prevAll(".input-group-addon, .input-group-btn")
+		// 		.prependTo($(this).find(".ab-input-group"));
+		//
+		// 	$(this)
+		// 		.closest(".ab-region")
+		// 		.find(".ab-btn-dropdown").children()
+		// 		.prependTo($(this).find(".ab-input-group .input-group-btn"));
+		//
+		// 	$(this)
+		// 		.nextAll(".input-group-addon, .input-group-btn")
+		// 		.appendTo($(this).find(".ab-input-group"));
+		//
+		// 	$(this)
+		// 		.closest(".ab-region")
+		// 		.find(".ab-btn-dropdown").children()
+		// 		.appendTo($(this).find(".ab-input-group .input-group-btn"));
+		// });
+
+		// $(".input-group-addon, .input-group-btn").closest(".ab-input-group").addClass("input-group");
+
+		// Region Cards
+		// $(".card.ab-card-img-top, .card.ab-card-img-overlay").each(function() {
+		// 	$(this).prepend($(this).find("img"));
+		// });
+		//
+		// $(".card.ab-card-img-bottom").each(function() {
+		// 	$(this).append($(this).find("img"));
+		// });
+		//
+		// // Report Cards
+		// $(".ab-card-img-top .card, .ab-card-img-overlay .card").each(function() {
+		// 	$(this).prepend($(this).find("img"));
+		// });
+		//
+		// $(".ab-card-img-bottom .card").each(function() {
+		// 	$(this).append($(this).find("img"));
+		// });
+	}
+};
+
 $(function() {
-    // replace navbar brand
-    $(".ab-header nav.navbar").prepend($(".ab-header a.navbar-brand").removeClass("hidden-xs-up"));
-
-    // Carousel
-    $(".carousel").each(function() {
-        var carousel = $(this);
-
-        carousel.find(".carousel-item").each(function(index) {
-            var attributes = (index === 0) ? "class=\"active\"" : "";
-            carousel.find(".carousel-indicators").append("<li " + attributes + " data-target=\"#" + $(carousel).attr("id") + "\" data-slide-to=\"" + index + "\"></li>");
-        });
-    });
-
-    // Collapsible
-    $(".ab-region.collapse").attr({
-        "role": "tablist",
-        "aria-multiselectable": "true"
-    });
-
-    $("button[target]").each(function() {
-        if ($("#" + $(this).attr("target")).length > 0) {
-            $(this).attr({
-                "data-toggle": "collapse",
-                "data-target": "#" + $(this).attr("target"),
-                "aria-expanded": "false",
-                "aria-controls": $(this).attr("target")
-            });
-        }
-    });
-
-    // Tooltips
-    $(".ab-field-tooltip [data-help][data-help!=''], .ab-button-tooltip[data-help][data-help!='']").each(function() {
-        $(this).attr({
-            "data-toggle": "tooltip",
-            "title": $(this).data("help")
-        });
-    });
-
-    // Popovers
-    $(".ab-field-popover [data-help][data-help!='']").each(function() {
-        $(this).attr({
-            "data-toggle": "popover",
-            "data-content": $(this).data("help"),
-            "title": $(this).text()
-        });
-    });
-
-    $(".ab-button-popover[data-help][data-help!='']").each(function() {
-        $(this).attr({
-            "data-toggle": "popover",
-            "data-content": $(this).data("help")
-        });
-    });
-
-    // Pushpin
-    $('.ab-sticky .navbar').each(function() {
-        $(this).pushpin({
-            top: $(this).offset().top,
-            offset: $(".ab-navbar-fixed-top .ab-header .navbar").outerHeight()
-        });
-    });
-
-    // Single Button Dropdowns
-    $(".ab-single-button-dropdown").each(function() {
-        $(this)
-            .removeAttr("onclick")
-            .addClass("dropdown-toggle")
-            .attr({
-                "data-toggle":"dropdown",
-                "aria-haspopup": "true",
-                "aria-expanded": "false",
-            })
-            .prependTo($(this).closest(".ab-region").find(".ab-btn-dropdown"));
-    });
-
-    // Split Button Dropdowns
-    $(".ab-split-button-dropdown").each(function() {
-        var classes = ["btn-primary", "btn-secondary", "btn-success", "btn-info", "btn-warning", "btn-danger", "btn-primary-outline", "btn-secondary-outline", "btn-success-outline", "btn-info-outline", "btn-warning-outline", "btn-danger-outline"];
-
-        var elementClasses = $(this).attr("class").split(" ");
-
-        for (var i = 0; i < elementClasses.length; i++) {
-            if ($.inArray(elementClasses[i], classes) >= 0) {
-                $thisClass = elementClasses[i];
-                break;
-            }
-        }
-
-        var toggle = '<button type="button" class="btn ' + $thisClass + ' dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="sr-only">Toggle Dropdown</span></button>';
-
-        $(this).prependTo($(this).closest(".ab-region").find(".ab-btn-dropdown").prepend(toggle));
-    });
-
-    // Label
-    $(".input-field label").each(function() {
-        $(this).appendTo($(this).parent());
-    });
-
-    // Input Groups
-    $(".form-group").not(":only-child").each(function() {
-        $(this)
-            .prevAll(".input-group-addon, .input-group-btn")
-            .prependTo($(this).find(".ab-input-group"));
-
-        $(this)
-            .closest(".ab-region")
-            .find(".ab-btn-dropdown").children()
-            .prependTo($(this).find(".ab-input-group .input-group-btn"));
-
-        $(this)
-            .nextAll(".input-group-addon, .input-group-btn")
-            .appendTo($(this).find(".ab-input-group"));
-
-        $(this)
-            .closest(".ab-region")
-            .find(".ab-btn-dropdown").children()
-            .appendTo($(this).find(".ab-input-group .input-group-btn"));
-    });
-
-    $(".input-group-addon, .input-group-btn").closest(".ab-input-group").addClass("input-group");
-
-    // Region Cards
-    $(".card.ab-card-img-top, .card.ab-card-img-overlay").each(function() {
-        $(this).prepend($(this).find("img"));
-    });
-
-    $(".card.ab-card-img-bottom").each(function() {
-        $(this).append($(this).find("img"));
-    });
-
-    // Report Cards
-    $(".ab-card-img-top .card, .ab-card-img-overlay .card").each(function() {
-        $(this).prepend($(this).find("img"));
-    });
-
-    $(".ab-card-img-bottom .card").each(function() {
-        $(this).append($(this).find("img"));
-    });
+	apexBootstrap.theme.init();
+	apexBootstrap.theme.bindings();
 });
--- src/js/bootstrap-init.js
+++ src/js/bootstrap-init.js
@@ -2,12 +2,5 @@
 Bootstrap Initialization
 ************/
 $(function() {
-    // Tooltips
-    $('[data-toggle="tooltip"]').tooltip(appSettings.tooltip);
-    // Popovers
-    $('[data-toggle="popover"]').popover(appSettings.popover);
-    // Scrollspy
-    $(".ab-list-scrollspy").each(function(){
-        $('body').scrollspy({ target: '#' + $(this).attr("id") });
-    });
+
 });
