9bb70afacc1da98adda1ccdc2dc5ce429d033cfa
Vincent Morneau <vincent.morneau@gmail.com>
2018-04-10 09:13:19 -0400

    more page template work

--- src/js/components/theme.js
+++ src/js/components/theme.js
@@ -8,11 +8,23 @@ module.exports = {
 		$('html').attr('style', 'visibility: visible!important');
 
 		$('[data-toggle="ft-main-left"]').on('click', () => {
-			$('.ft-main-left').toggleClass('open');
+			if ($('.ft-main-left').hasClass('open')) {
+				$('.ft-main-left').removeClass('open');
+				$('.ft-main-center').removeClass('ft-main-left-open');
+			} else {
+				$('.ft-main-left').addClass('open');
+				$('.ft-main-center').addClass('ft-main-left-open');
+			}
 		});
 
 		$('[data-toggle="ft-main-right"]').on('click', () => {
-			$('.ft-main-right').toggleClass('open');
+			if ($('.ft-main-right').hasClass('open')) {
+				$('.ft-main-right').removeClass('open');
+				$('.ft-main-center').removeClass('ft-main-right-open');
+			} else {
+				$('.ft-main-right').addClass('open');
+				$('.ft-main-center').addClass('ft-main-right-open');
+			}
 		});
 	}
 };
