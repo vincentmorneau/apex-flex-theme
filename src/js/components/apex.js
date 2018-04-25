5acab65e6ceeb7ce3994087bc464617cbd2fd5bf
Vincent Morneau <vincent.morneau@gmail.com>
2018-04-25 16:25:17 -0400

    responsive sidebars

--- src/js/components/apex.js
+++ src/js/components/apex.js
@@ -11,18 +11,25 @@ module.exports = {
 	},
 
 	navbar() {
+		$('.ft-breadcrumb').css({
+			top: ft.theme.headerHeight()
+		});
+
 		$('.ft-navbar-fixed-top .ft-main').css({
 			'margin-top': ft.theme.defaultStickyTop()
 		});
 
-		$('.ft-navbar-fixed-top .ft-main-left, .ft-navbar-fixed-top .ft-main-right, .ft-breadcrumb').css({
-			top: ft.theme.headerHeight()
+		const offset = (ft.theme.getCurrentViewport() == 'xs' ? ft.theme.defaultStickyTop() : ft.theme.headerHeight());
+
+		$('.ft-navbar-fixed-top .ft-main-left, .ft-navbar-fixed-top .ft-main-right').css({
+			height: 'calc(100vh - ' + offset + 'px)',
+			top: offset
 		});
 
-		$('.ft-main-left, .ft-main-right').css({
-			height: 'calc(100vh - ' + ft.theme.headerHeight() + 'px)',
-			top: ft.theme.headerHeight()
-		});
+		// $('.ft-main-left, .ft-main-right').css({
+		// 	height: 'calc(100vh - ' + ft.theme.headerHeight() + 'px)',
+		// 	top: ft.theme.headerHeight()
+		// });
 	},
 
 	rds() {
