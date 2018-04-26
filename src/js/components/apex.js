3f39d54c6fa281046063abe31159aadd1ae77d4e
Vincent Morneau <vincent.morneau@gmail.com>
2018-04-26 16:02:10 -0400

    buttons and cleanup

--- src/js/components/apex.js
+++ src/js/components/apex.js
@@ -15,21 +15,16 @@ module.exports = {
 			top: ft.theme.headerHeight()
 		});
 
-		$('.ft-navbar-fixed-top .ft-main').css({
+		$('.ft-main').css({
 			'margin-top': ft.theme.defaultStickyTop()
 		});
 
-		const offset = (ft.theme.getCurrentViewport() == 'xs' ? ft.theme.defaultStickyTop() : ft.theme.headerHeight());
+		const offset = (ft.theme.getCurrentViewport() === 'xs' ? ft.theme.defaultStickyTop() : ft.theme.headerHeight());
 
-		$('.ft-navbar-fixed-top .ft-main-left, .ft-navbar-fixed-top .ft-main-right').css({
+		$('.ft-main-left, .ft-main-right').css({
 			height: 'calc(100vh - ' + offset + 'px)',
 			top: offset
 		});
-
-		// $('.ft-main-left, .ft-main-right').css({
-		// 	height: 'calc(100vh - ' + ft.theme.headerHeight() + 'px)',
-		// 	top: ft.theme.headerHeight()
-		// });
 	},
 
 	rds() {
